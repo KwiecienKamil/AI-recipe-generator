97df8760ed9fc0d8d3d2f62c9ef3e434bc0a36d9
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2025-02-07 21:47:30 +0100

    Generating recipes fixes

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { CgShapeHalfCircle } from "react-icons/cg";
+import { SiCircleci } from "react-icons/si";
 
 const page = () => {
   const [userInput, setUserInput] = useState("");
@@ -35,21 +36,38 @@ const page = () => {
   };
 
   return (
-    <div className="h-full font-montserrat pt-24">
-      <div className="flex flex-col justify-center">
-        <textarea
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-          placeholder="Enter your recipe prompt here..."
-          className="p-4"
-        />
+    <div className="flex flex-col justify-center items-center font-montserrat pt-24">
+      <div className="w-[20rem] pb-8">
+        <div className="flex flex-col justify-center">
+          <textarea
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            placeholder="Enter your recipe prompt here..."
+            className="p-4"
+          />
+          <button
+            onClick={handleGenerateRecipes}
+            className="px-4 py-2 bg-black text-white hover:brightness-75 duration-300 text-center"
+          >
+            {loading ? (
+              <span className="text-center">
+                <SiCircleci className="animate-spin" />
+              </span>
+            ) : (
+              <span>Generate recipes</span>
+            )}
+          </button>
+        </div>
       </div>
       {recipes && (
         <div>
           {recipes && (
-            <div className="grid grid-cols-3 gap-2">
+            <div
+              className="grid grid-cols-3 gap-2"
+              key={Math.floor(Math.random() * 999)}
+            >
               {recipes.map((recipe: any) => (
-                <div className="p-2 bg-white/40 max-w-96 rounded-xl ">
+                <div className="p-2 bg-white/40 max-w-96 rounded-xl">
                   <h4>{recipe.name}</h4>
                   <p>{recipe.description}</p>
                   <p>{recipe.instructions}</p>
@@ -59,16 +77,6 @@ const page = () => {
           )}
         </div>
       )}
-      <button
-        onClick={handleGenerateRecipes}
-        className="px-4 py-2 bg-black text-white hover:brightness-75 duration-300 text-center"
-      >
-        {loading ? (
-          <CgShapeHalfCircle className="animate-spin" />
-        ) : (
-          <span>Generate recipes</span>
-        )}
-      </button>
     </div>
   );
 };
